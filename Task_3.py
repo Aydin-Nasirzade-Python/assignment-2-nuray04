c44e745bbb641d271ea44b76dd79017b22f4e07a
nuray04 <165520762+nuray04@users.noreply.github.com>
2024-04-07 23:59:27 +0400

    Update Task_3.py

--- Task_3.py
+++ Task_3.py
@@ -3,18 +3,18 @@
 def main():
  uzunluq = int(input("Enter the wavelength in nm: "))
  if 380 <= uzunluq <= 750:
-     if 380 <= uzunluq < 450:
-         print("The relevant color is Violet")
-     elif 450 <= uzunluq < 495:
-         print("The relevant color is Blue")
-    elif 495 <= uzunluq < 570:
-         print("The relevant color is Green")
-    elif 570 <= uzunluq < 590:
-         print("The relevant color is Yellow")
-    elif 590 <= uzunluq < 620:
-         print("The relevant color is Orange")
-    else:
-         print("The relevant color is Red")
+  if 380 <= uzunluq < 450:
+   print("The relevant color is Violet")
+  elif 450 <= uzunluq < 495:
+    print("The relevant color is Blue")
+   elif 495 <= uzunluq < 570:
+     print("The relevant color is Green")
+   elif 570 <= uzunluq < 590:
+     print("The relevant color is Yellow")
+   elif 590 <= uzunluq < 620:
+     print("The relevant color is Orange")
+   else:
+     print("The relevant color is Red")
  else:
      print("Invalid input!")
   pass
